1be5120ae8ada2e5ecba9678d1becb6fae6ca004
naipia <39188756+naipia@users.noreply.github.com>
2020-04-18 19:03:50 +0900

    Fix build failures when running tests on macOS

--- src/languages.ts
+++ src/languages.ts
@@ -9,12 +9,12 @@ interface LangInfo {
 export const languages: LangInfo = {
   'C++': {
     extension: 'cpp',
-    command: 'g++ %S && ./a.out < %IN > %OUT',
+    command: 'g++ -o /tmp/main %S && /tmp/main < %IN > %OUT',
     atcoderID: 'text/x-c++src',
   },
   Go: {
     extension: 'go',
-    command: 'go build -o ./main %S && ./main < %IN > %OUT',
+    command: 'go build -o /tmp/main %S && /tmp/main < %IN > %OUT',
     atcoderID: 'text/x-go',
   },
 };
